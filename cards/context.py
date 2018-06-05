6a21e201f369d5a471e431a201024d8ac7388797
Erick Bazán <bazan.erick@gmail.com>
2018-06-05 17:25:20 -0500

    fix(alerts):

--- cards/context.py
+++ cards/context.py
@@ -2,5 +2,6 @@ from .models import Notification
 
 
 def departments(request):
-    notifications = Notification.objects.filter(receiver=request.user, receiver_read=False)
-    return {'alerts': notifications}
+    if request.user.is_authenticated:
+        notifications = Notification.objects.filter(receiver=request.user, receiver_read=False)
+        return {'alerts': notifications}
