151c0b82f55682ac5cc22a4b2101e38823e6e983
Erick Bazán <bazan.erick@gmail.com>
2018-06-11 13:10:10 -0500

    fix(admin/events): fix date

--- events/admin.py
+++ events/admin.py
@@ -5,4 +5,4 @@ from .models import Match
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('id', 'home_team', 'away_team', 'away_result', 'home_result', )
-    fields = ('home_team', 'away_team', 'away_result', 'home_result',)
+    fields = ('home_team', 'away_team', 'away_result', 'home_result', 'active_until',)
