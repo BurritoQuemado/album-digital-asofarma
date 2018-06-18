a9dc1889036f203117b8d21e7cd5709d89aabc8b
Erick Bazán <bazan.erick@gmail.com>
2018-06-18 10:49:44 -0500

    feat(events/admin): add queryset

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Match
+from .models import Match, Prediction
 
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('id', 'home_team', 'away_team', 'away_result', 'home_result', )
     fields = ('home_team', 'away_team', 'away_result', 'home_result', 'active_until',)
+
+
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user',)
+
+    def get_queryset(self, request):
+        qs = super(PredictionAdmin, self).get_queryset(request)
+        match = Match.objects.all().first()
+        return qs.filter(away_result=match.away_result, home_result=match.home_result)
