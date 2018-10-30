067d6ee19e2fc460a053e197221ae5fd429232b0
Erick Bazán <bazan.erick@gmail.com>
2018-10-30 16:13:06 -0600

    fix(frontend): fix server with eslint

--- frontend/webpack/server.js
+++ frontend/webpack/server.js
@@ -1,16 +1,16 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./config.dev.babel');
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./config.dev.babel')
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  inline: true,
-  historyApiFallback: true,
-  headers: { 'Access-Control-Allow-Origin': '*' }
-}).listen(3000, '0.0.0.0', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at 0.0.0.0:3000');
-});
+    publicPath: config.output.publicPath,
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+}).listen(3000, '0.0.0.0', (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Listening at 0.0.0.0:3000')
+})
